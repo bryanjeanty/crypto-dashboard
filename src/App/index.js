e29e61b0cf57f528130272095756760aa958c2e6
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-04-14 20:18:19 -0400

    page content component created to account for fetching load

--- src/App/index.js
+++ src/App/index.js
@@ -4,6 +4,7 @@ import Settings from "../settings";
 import AppLayout from "./AppLayout";
 import AppBar from "./AppBar";
 import AppProvider from "./AppProvider";
+import Content from "../shared/Content";
 
 class App extends Component {
   render() {
@@ -11,7 +12,9 @@ class App extends Component {
       <AppLayout>
         <AppProvider>
           <AppBar />
-          <Settings />
+          <Content>
+            <Settings />
+          </Content>
         </AppProvider>
       </AppLayout>
     );
