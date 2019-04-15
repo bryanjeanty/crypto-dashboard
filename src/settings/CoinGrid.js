d534f6d536966ebe466ad3b88d345aff19a8e063
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-04-15 02:28:25 -0400

    coinTile and coinImage added

--- src/settings/CoinGrid.js
+++ src/settings/CoinGrid.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { AppContext } from "../App/AppProvider";
 import styled from "styled-components";
-import { SelectableTile } from "../shared/Tile";
+import CoinTile from "./CoinTile";
 
 export const CoinGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 15px;
+  margin-top: 40px;
 `;
+
+getCoinsToDisplay = coinList => {
+  return Object.keys(coinList).slice(0, 100);
+};
 
 export default () => {
   return (
@@ -15,8 +20,8 @@ export default () => {
       {({ coinList }) => {
         return (
           <CoinGridStyled>
-            {Object.keys(coinList).map(coinKey => {
-              return <SelectableTile>{coinKey}</SelectableTile>;
+            {getCoinsToDisplay(coinList).map(coinKey => {
+              return <CoinTile coinKey={coinKey} />;
             })}
           </CoinGridStyled>
         );
